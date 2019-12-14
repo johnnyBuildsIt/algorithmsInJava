128087286b213a61cd7c7d269a3bef17b4802981
johnnyBuildsIt <bluegrassandclimbing@hotmail.com>
2019-12-14 17:19:20 -0500

    binary search implemented

--- src/main/java/algoExpert/binarySearch/BinarySearch.java
+++ src/main/java/algoExpert/binarySearch/BinarySearch.java
@@ -1,7 +1,26 @@
 package algoExpert.binarySearch;
-
+/*
+    Write a function that takes in a sorted array of integers as well as a target integer.
+    The function should use the Binary Search algorithm to find if the target number is
+    contained in the array and should return its index if it is, otherwise -1;
+ */
 public class BinarySearch {
     public static int binarySearch(int[] array, int target) {
-        return 0;
+        int i = 0;
+        int m = 0;
+        int j = array.length - 1;
+
+        while(i <= j) {
+            m = (i + j) / 2;
+            if(array[m] == target) {
+                return m;
+            } else if(array[m] > target) {
+                j = m - 1;
+            } else {
+                i = m + 1;
+            }
+        }
+
+        return -1;
     }
 }
--- src/test/java/algoExpert/binarySearch/BinarySearchTest.java
+++ src/test/java/algoExpert/binarySearch/BinarySearchTest.java
@@ -1,4 +1,28 @@
 package algoExpert.binarySearch;
 
+import org.junit.jupiter.api.Test;
+
+
+import static algoExpert.binarySearch.BinarySearch.binarySearch;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BinarySearchTest {
+
+    @Test
+    void shouldReturnNegativeOneForEmptyArray() {
+        int[] searchList = new int[] {};
+        assertTrue(binarySearch(searchList, 4) == -1);
+    }
+
+    @Test
+    void shouldReturnZeroForSingleItemInArray() {
+        int[] searchList = new int[] {4};
+        assertTrue(binarySearch(searchList, 4) == 0);
+    }
+
+    @Test
+    void shouldFindNumber() {
+        int[] searchList = new int[] {0, 2, 3, 7, 10};
+        assertTrue(binarySearch(searchList, 7) == 3);
+    }
 }
